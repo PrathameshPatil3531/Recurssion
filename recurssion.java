2ed60f7397df70648c25dece00ad1254096391f6
Prtham <prathamrajput3531@gmail.com>
2026-04-02 23:32:22 +0530

    Tiling problem

--- recurssion.java
+++ recurssion.java
@@ -4,14 +4,28 @@ public class recurssion {
         if(p == 1){
             return n;
         }else{
-            return  n * pow(n , p-1);
+          if(p %2 ==0 ){
+            return pow(n , p/2) *pow(n , p/2);
+          }else{
+            return n *pow(n , p/2) *pow(n , p/2);
+          }
         }
        
     }
 
+    public static int tiling(int n){
+      if ( n == 0 || n ==1){
+        return 1;
+
+      }
+      int fnm1 = tiling((n-1));
+      int fnm2 = tiling(n-2);
+      return fnm1 + fnm2;
+    }
+
     public static void main (String args[]){
 
-        System.out.println(pow(2, 5));
+        System.out.println(tiling(5));
 
     }
     
